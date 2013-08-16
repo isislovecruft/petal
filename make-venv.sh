12e01635d951b61927e584b287af572869591d3f
Isis Lovecruft <isis@torproject.org>
2013-08-16 18:24:54 +0000

    Source virtualenvwrapper no matter where it is installed.

--- make-venv.sh
+++ make-venv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#-------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 # make-venv.sh
 # ------------
 # Create a Python virtualenv with virtualenvwrapper for petal.
 # 
 # :authors: Isis Agora Lovecruft, 0xa3adb67a2cdb8b35
-# :license: maybe AGPLv3, not sure yet
+# :license: Three-Clause BSD
 # :version: 0.0.1
-#-------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 
-source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv -a "$PWD" --no-site-packages --unzip-setuptools --distribute petal
+source `which virutualenvwrapper.sh`
+mkvirtualenv -a "$PWD" --unzip-setuptools --setuptools petal
